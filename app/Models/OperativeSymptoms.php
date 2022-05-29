a9c99c6d5f643ba03700663868b307d048d48846
siyamkumar <siyamkumar@gmail.com>
2022-05-30 03:39:12 +0530

    Added Models - Physical Examination & Operative Symptoms

---
+++ app/Models/OperativeSymptoms.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OperativeSymptoms extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'case_report_form_id',
+        'pre_operative_data_id',
+        'symptoms',
+        'angina', 'angina_class', 'angina_duration',
+        'dyspnea', 'dyspnea_class', 'dyspnea_duration',
+        'syncope', 'syncope_duration',
+        'palpitation', 'palpitation_duration',
+        'giddiness', 'giddiness_duration',
+        'fever', 'fever_duration',
+        'heart_failure_admission', 'heart_failure_admission_duration',
+        'others', 'others_text', 'others_duration'
+
+    ];
+
+    protected $casts = [
+        'angina_duration' => 'array',
+        'dyspnea_duration' => 'array',
+        'syncope_duration' => 'array',
+        'palpitation_duration' => 'array',
+        'giddiness_duration' => 'array',
+        'fever_duration' => 'array',
+        'heart_failure_admission_duration' => 'array',
+        'others_duration' => 'array',
+    ];
+
+    public function preoperatives(){
+        return $this->belongsTo(PreOperativeData::class);
+    }
+}
---
+++ app/Models/PhysicalExamination.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PhysicalExamination extends Model
+{
+    use HasFactory;
+protected $fillable = [
+    'case_report_form_id' ,
+    'pre_operative_data_id' ,
+    'post_operative_data_id' ,
+    'scheduled_visits_id' ,
+    'unscheduled_visits_id' ,
+    'height',
+    'weight',
+    'bsa',
+    'heart_rate',
+    'systolic_bp',
+    'diastolic_bp',
+];
+    public function preoperative(){
+        return $this->belongsTo(PreOperativeData::class);
+    }
+
+    public function postoperative(){
+        return $this->belongsTo(PostOperativeData::class);
+    }
+
+}
+
+
