682f41e9fc8878739aba5667d77b654afaa244d5
siyamkumar <siyamkumar@gmail.com>
2022-05-30 03:38:20 +0530

    Policy Registered for Roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\CaseReportForm;
+use App\Models\CaseReportFormVisit;
+use App\Models\Roles;
+use App\Observers\CaseReportFormObserver;
+use App\Observers\CaseReportFormVisitObserver;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +30,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        $this->registerRolePolicy();
+        
+        Paginator::useBootstrapFive();
+
+        CaseReportForm::observe(CaseReportFormObserver::class);
+       
+    }
+
+    public function registerRolePolicy()
+    {
+        $allRoles = Roles::all();
+        foreach ($allRoles as $role) {
+
+            Gate::define($role->slug, function ($user) use ($role) {
+                return $user->role_id == $role->id;
+
+            });
+        }
     }
 }
