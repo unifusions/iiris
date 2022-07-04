923e1e07a5e097a58fd963c3df7207a47f69f70b
siyamkumar <siyamkumar@gmail.com>
2022-07-04 17:50:38 +0530

    prod 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,11 +20,11 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .react()
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ])
+    // .postCss('resources/css/app.css', 'public/css', [
+    //     require('postcss-import'),
+    //     require('tailwindcss'),
+    //     require('autoprefixer'),
+    // ])
     .alias({
         '@': 'resources/js',
     });
