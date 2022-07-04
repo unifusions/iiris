b8bdd41179561db78fd0d95d53e7ca4a6edf4a95
siyamkumar <siyamkumar@gmail.com>
2022-07-04 17:06:54 +0530

    Production

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,14 +22,13 @@
 
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+    <link rel="stylesheet" href="{{asset(mix('css/app.css'))}}">
     <!-- Scripts -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer>
     </script>
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
 
     @inertiaHead
 </head>
