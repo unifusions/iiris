831c3c8c8aacd6c2c7a5ee04e29d43515dc7a8d7
unifusions <siyam@unifusions.com>
2026-01-21 16:05:51 +0530

    CSRF Test

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,11 +36,4 @@ window.axios.defaults.withCredentials = true;
 window.axios.defaults.withXSRFToken = true;
 
 
-const token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found in <meta>');
-}
-
- 
+  
