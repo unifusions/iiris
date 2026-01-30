08412a0fb1c558e2c3c46b0b3ee1a412d0e3c0ce
unifusions <siyam@unifusions.com>
2026-01-30 18:10:29 +0530

    Fixed back function in Electrocardiogram

--- public/build/assets/divWithClassName-CoApbZDL.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as m}from"./app-C69FMBm-.js";import{c as o}from"./ThemeProvider-Dkhr7Shq.js";const i=a=>e.forwardRef((s,r)=>m.jsx("div",{...s,ref:r,className:o(s.className,a)}));export{i as d};
---
+++ public/build/assets/divWithClassName-DHcE-igb.js
@@ -0,0 +1,1 @@
+import{r as e,j as m}from"./app-D9gKO3l3.js";import{c as o}from"./ThemeProvider--yaTtye1.js";const i=a=>e.forwardRef((s,r)=>m.jsx("div",{...s,ref:r,className:o(s.className,a)}));export{i as d};
